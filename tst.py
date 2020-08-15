def331c2f71f477a2405ec80b88c35dcd42fccf2
lnthong1995 <69614613+lnthong1995@users.noreply.github.com>
2020-08-15 09:55:24 +0700

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,5 +1,5 @@
 import json
-facts = '{"hostname": "nxosv", "os": "7.3", "IP": "192.168.1.1"}'
+facts = '{"hostname": "Router_1", "os": "7.3", "interface f0/0": { "IP": "192.168.1.1", "netmask":"255.255.255.0", "status":"up"}}'
 factsd = json.loads(facts)
 print(factsd)
 print(json.dumps(factsd, indent=4))
