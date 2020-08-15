86de6765d2150f4a2577b17a11c283590dc20667
lnthong1995 <69614613+lnthong1995@users.noreply.github.com>
2020-08-15 09:44:44 +0700

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,5 +1,5 @@
 import json
-facts = '{"hostname": "nxosv", "os": "7.3", "IP": "192.168.1.1"}'
+facts = '{"hostname": "R1", "IP": "192.168.1.1", "Netmask": "255.255.255.0"}'
 factsd = json.loads(facts)
 print(factsd)
 print(json.dumps(factsd, indent=4))
